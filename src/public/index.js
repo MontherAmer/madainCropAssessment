bf6cc92d6c1632c38c77bde8eb0a1a29096a0480
montheramer <monther.amer48@gmail.com>
2022-03-23 18:29:20 +0200

    display cards on UI

--- src/public/index.js
+++ src/public/index.js
@@ -7,7 +7,34 @@ let fetchDataFromAPI = async () => {
 
   let data = await response.json();
   store = data;
+
   console.log('MY DATA: ', store);
+  showTheUI(data);
 };
 
 fetchDataFromAPI();
+
+let showTheUI = (list) => {
+  let cardsDiv = document.querySelector('#cards');
+  cardsDiv.innerHTML = '';
+
+  list.map((item) => {
+    let newDiv = document.createElement('div');
+
+    newDiv.innerHTML = `
+    <div class="card">
+      <span class=""> ${item.fname.charAt(0)} ${item.lname.charAt(0)} </span>
+      <div class="name-container">
+        <p>${item.fname} &nbsp;</p>
+        <p>${item.lname}</p>
+      </div>
+      <div class="card-cat">
+        <label class="cat">${item.category}</label>
+      </div>
+    </div>`;
+
+    console.log('cardsDiv ', cardsDiv);
+
+    cardsDiv.appendChild(newDiv);
+  });
+};
