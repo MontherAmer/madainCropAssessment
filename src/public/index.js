e333a8986ccd1b2ef28cadcdd4ac48d0bb688628
montheramer <monther.amer48@gmail.com>
2022-03-23 17:37:55 +0200

    fetch data from API

--- src/public/index.js
+++ src/public/index.js
@@ -1,1 +1,13 @@
-console.log('JS is working');
+let store = [];
+
+let fetchDataFromAPI = async () => {
+  let response = await fetch(
+    'https://filltext.com/?rows=10&fname={firstName}&lname={lastName}&category=[%22categroy1%22,%22category2%22,%22categroy3%22]&pretty=true'
+  );
+
+  let data = await response.json();
+  store = data;
+  console.log('MY DATA: ', store);
+};
+
+fetchDataFromAPI();
